579ace8f376dbd52ab2515a870b6048f0e123927
superdinmc <superdinmc@gmail.com>
2023-12-22 11:29:18 +0700

    fix again

--- src/commands/slash/initdm.js
+++ src/commands/slash/initdm.js
@@ -4,6 +4,6 @@ module.exports = {
         .setName('init-dm')
         .setDescription('Make bot be able to evaluate code in your DM.'),
     run: (client, interaction) => {
-        return interaction.user.createDM().then(interaction.reply.bind(null,[{content:'Successfully bound DM handler to your DM!',ephemeral:!!interaction.guild}]));
+        return interaction.user.createDM().then(()=>interaction.reply({content:'Successfully bound DM handler to your DM!',ephemeral:!!interaction.guild}));
     }
 }
